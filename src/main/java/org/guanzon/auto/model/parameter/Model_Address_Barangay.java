8b74e55ef00569bb426729c5ad5009e8d10b3c01
r-sie <32829606+Arsiela@users.noreply.github.com>
2024-11-06 17:09:17 +0800

    11062024 - Added functionality for Barangay, Town and Province
    11062024 - Added functionality for Barangay, Town and Province

---
+++ src/main/java/org/guanzon/auto/model/parameter/Model_Address_Barangay.java
@@ -0,0 +1,633 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.guanzon.auto.model.parameter;
+
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Types;
+import java.util.Date;
+import javax.sql.rowset.CachedRowSet;
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.appdriver.base.SQLUtil;
+import org.guanzon.appdriver.constant.EditMode;
+import org.guanzon.appdriver.constant.RecordStatus;
+import org.guanzon.appdriver.iface.GEntity;
+import org.json.simple.JSONObject;
+
+/**
+ *
+ * @author Arsiela
+ */
+public class Model_Address_Barangay implements GEntity {
+    final String XML = "Model_Address_Barangay.xml";
+    
+    GRider poGRider;                //application driver
+    CachedRowSet poEntity;          //rowset
+    JSONObject poJSON;              //json container
+    int pnEditMode;                 //edit mode
+    String psExclude = "sProvIDxx»sProvName»sTownName»sZippCode"; //»
+
+    /**
+     * Entity constructor
+     *
+     * @param foValue - GhostRider Application Driver
+     */
+    public Model_Address_Barangay(GRider foValue) {
+        if (foValue == null) {
+            System.err.println("Application Driver is not set.");
+            System.exit(1);
+        }
+
+        poGRider = foValue;
+
+        initialize();
+    }
+    
+    private void initialize() {
+        try {
+            poEntity = MiscUtil.xml2ResultSet(System.getProperty("sys.default.path.metadata") + XML, getTable());
+
+            poEntity.last();
+            poEntity.moveToInsertRow();
+
+            MiscUtil.initRowSet(poEntity);
+            poEntity.updateString("cRecdStat", RecordStatus.ACTIVE);
+
+            poEntity.insertRow();
+            poEntity.moveToCurrentRow();
+
+            poEntity.absolute(1);
+
+            pnEditMode = EditMode.UNKNOWN;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Gets the column index name.
+     *
+     * @param fnValue - column index number
+     * @return column index name
+     */
+    @Override
+    public String getColumn(int fnValue) {
+        try {
+            return poEntity.getMetaData().getColumnLabel(fnValue);
+        } catch (SQLException e) {
+        }
+        return "";
+    }
+
+    /**
+     * Gets the column index number.
+     *
+     * @param fsValue - column index name
+     * @return column index number
+     */
+    @Override
+    public int getColumn(String fsValue) {
+        try {
+            return MiscUtil.getColumnIndex(poEntity, fsValue);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Gets the total number of column.
+     *
+     * @return total number of column
+     */
+    @Override
+    public int getColumnCount() {
+        try {
+            return poEntity.getMetaData().getColumnCount();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return -1;
+    }
+
+    @Override
+    public int getEditMode() {
+        return pnEditMode;
+    }
+
+    @Override
+    public String getTable() {
+        return "barangay";
+    }
+
+    /**
+     * Gets the value of a column index number.
+     *
+     * @param fnColumn - column index number
+     * @return object value
+     */
+    @Override
+    public Object getValue(int fnColumn) {
+        try {
+            return poEntity.getObject(fnColumn);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Gets the value of a column index name.
+     *
+     * @param fsColumn - column index name
+     * @return object value
+     */
+    @Override
+    public Object getValue(String fsColumn) {
+        try {
+            return poEntity.getObject(MiscUtil.getColumnIndex(poEntity, fsColumn));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Sets column value.
+     *
+     * @param fnColumn - column index number
+     * @param foValue - value
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject setValue(int fnColumn, Object foValue) {
+        try {
+            poJSON = MiscUtil.validateColumnValue(System.getProperty("sys.default.path.metadata") + XML, MiscUtil.getColumnLabel(poEntity, fnColumn), foValue);
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+
+            poEntity.updateObject(fnColumn, foValue);
+            poEntity.updateRow();
+
+            poJSON = new JSONObject();
+            poJSON.put("result", "success");
+            poJSON.put("value", getValue(fnColumn));
+        } catch (SQLException e) {
+            e.printStackTrace();
+            poJSON.put("result", "error");
+            poJSON.put("message", e.getMessage());
+        }
+
+        return poJSON;
+    }
+
+    /**
+     * Sets column value.
+     *
+     * @param fsColumn - column index name
+     * @param foValue - value
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject setValue(String fsColumn, Object foValue) {
+        poJSON = new JSONObject();
+
+        try {
+            return setValue(MiscUtil.getColumnIndex(poEntity, fsColumn), foValue);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            poJSON.put("result", "error");
+            poJSON.put("message", e.getMessage());
+        }
+        return poJSON;
+    }
+
+    /**
+     * Set the edit mode of the entity to new.
+     *
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject newRecord() {
+        pnEditMode = EditMode.ADDNEW;
+
+        //replace with the primary key column info
+        setBrgyID(MiscUtil.getNextCode(getTable(), "sBrgyIDxx", false, poGRider.getConnection(), ""));
+
+        poJSON = new JSONObject();
+        poJSON.put("result", "success");
+        return poJSON;
+    }
+
+    /**
+     * Opens a record.
+     *
+     * @param fsValue - filter values
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject openRecord(String fsValue) {
+        poJSON = new JSONObject();
+
+        String lsSQL = getSQL();;
+
+        //replace the condition based on the primary key column of the record
+        lsSQL = MiscUtil.addCondition(lsSQL, " a.sBrgyIDxx = " + SQLUtil.toSQL(fsValue));
+
+        ResultSet loRS = poGRider.executeQuery(lsSQL);
+
+        try {
+            if (loRS.next()) {
+                for (int lnCtr = 1; lnCtr <= loRS.getMetaData().getColumnCount(); lnCtr++) {
+                    setValue(lnCtr, loRS.getObject(lnCtr));
+                }
+
+                pnEditMode = EditMode.UPDATE;
+
+                poJSON.put("result", "success");
+                poJSON.put("message", "Record loaded successfully.");
+            } else {
+                poJSON.put("result", "error");
+                poJSON.put("message", "No record to load.");
+            }
+        } catch (SQLException e) {
+            poJSON.put("result", "error");
+            poJSON.put("message", e.getMessage());
+        }
+
+        return poJSON;
+    }
+
+    /**
+     * Save the entity.
+     *
+     * @return result as success/failed
+     */
+    @Override
+    public JSONObject saveRecord() {
+        poJSON = new JSONObject();
+
+        if (pnEditMode == EditMode.ADDNEW || pnEditMode == EditMode.UPDATE) {
+            String lsSQL;
+            if (pnEditMode == EditMode.ADDNEW) {
+                //replace with the primary key column info
+                setBrgyID(MiscUtil.getNextCode(getTable(), "sBrgyIDxx", false, poGRider.getConnection(), ""));
+                setModified(poGRider.getUserID());
+                setModifiedDte(poGRider.getServerDate());
+                lsSQL = MiscUtil.makeSQL(this, psExclude);
+
+                if (!lsSQL.isEmpty()) {
+                    if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), "") > 0) {
+                        poJSON.put("result", "success");
+                        poJSON.put("message", "Record saved successfully.");
+                    } else {
+                        poJSON.put("result", "error");
+                        poJSON.put("message", poGRider.getErrMsg());
+                    }
+                } else {
+                    poJSON.put("result", "error");
+                    poJSON.put("message", "No record to save.");
+                }
+            } else {
+                Model_Address_Barangay loOldEntity = new Model_Address_Barangay(poGRider);
+
+                //replace with the primary key column info
+                JSONObject loJSON = loOldEntity.openRecord(this.getBrgyID());
+
+                if ("success".equals((String) loJSON.get("result"))) {
+                    setModified(poGRider.getUserID());
+                    setModifiedDte(poGRider.getServerDate());
+                    //replace the condition based on the primary key column of the record
+                    lsSQL = MiscUtil.makeSQL(this, loOldEntity, "sBrgyIDxx = " + SQLUtil.toSQL(this.getBrgyID()), psExclude);
+
+                    if (!lsSQL.isEmpty()) {
+                        if (poGRider.executeQuery(lsSQL, getTable(), poGRider.getBranchCode(), "") > 0) {
+                            poJSON.put("result", "success");
+                            poJSON.put("message", "Record saved successfully.");
+                        } else {
+                            poJSON.put("result", "error");
+                            poJSON.put("message", poGRider.getErrMsg());
+                        }
+                    } else {
+                        poJSON.put("result", "success");
+                        poJSON.put("message", "No updates has been made.");
+                    }
+                } else {
+                    poJSON.put("result", "error");
+                    poJSON.put("message", "Record discrepancy. Unable to save record.");
+                }
+            }
+        } else {
+            poJSON.put("result", "error");
+            poJSON.put("message", "Invalid update mode. Unable to save record.");
+            return poJSON;
+        }
+
+        return poJSON;
+    }
+
+    /**
+     * Prints all the public methods used<br>
+     * and prints the column names of this entity.
+     */
+    @Override
+    public void list() {
+        Method[] methods = this.getClass().getMethods();
+
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("LIST OF PUBLIC METHODS FOR " + this.getClass().getName() + ":");
+        System.out.println("--------------------------------------------------------------------");
+        for (Method method : methods) {
+            System.out.println(method.getName());
+        }
+
+        try {
+            int lnRow = poEntity.getMetaData().getColumnCount();
+
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("ENTITY COLUMN INFO");
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("Total number of columns: " + lnRow);
+            System.out.println("--------------------------------------------------------------------");
+
+            for (int lnCtr = 1; lnCtr <= lnRow; lnCtr++) {
+                System.out.println("Column index: " + (lnCtr) + " --> Label: " + poEntity.getMetaData().getColumnLabel(lnCtr));
+                if (poEntity.getMetaData().getColumnType(lnCtr) == Types.CHAR
+                        || poEntity.getMetaData().getColumnType(lnCtr) == Types.VARCHAR) {
+
+                    System.out.println("Column index: " + (lnCtr) + " --> Size: " + poEntity.getMetaData().getColumnDisplaySize(lnCtr));
+                }
+            }
+        } catch (SQLException e) {
+        }
+
+    }
+    
+    /**
+     * Gets the SQL statement for this entity.
+     *
+     * @return SQL Statement
+     */
+    public String makeSQL() {
+        return MiscUtil.makeSQL(this, psExclude);
+    }
+    
+    /**
+     * Gets the SQL Select statement for this entity.
+     *
+     * @return SQL Select Statement
+     */
+    public String makeSelectSQL() {
+        return MiscUtil.makeSelect(this, psExclude);
+    }
+    
+    public String getSQL(){
+        return    " SELECT "                                            
+                + "    a.sBrgyIDxx "                                    
+                + "  , a.sBrgyName "                                    
+                + "  , a.sTownIDxx "                                    
+                + "  , a.cHasRoute "                                    
+                + "  , a.cBlackLst "                                    
+                + "  , a.cRecdStat "                                    
+                + "  , a.sModified "                                    
+                + "  , a.dModified "                                    
+                + "  , b.sTownName "                                    
+                + "  , b.sZippCode "                                    
+                + "  , c.sProvIDxx "                                    
+                + "  , c.sProvName "                                    
+                + " FROM barangay a"                                    
+                + " LEFT JOIN  towncity b ON b.sTownIDxx = a.sTownIDxx "
+                + " LEFT JOIN province c ON c.sProvIDxx = b.sProvIDxx  " ;   
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setBrgyID(String fsValue) {
+        return setValue("sBrgyIDxx", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getBrgyID() {
+        return (String) getValue("sBrgyIDxx");
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setBrgyName(String fsValue) {
+        return setValue("sBrgyName", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getBrgyName() {
+        return (String) getValue("sBrgyName");
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setTownID(String fsValue) {
+        return setValue("sTownIDxx", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getTownID() {
+        return (String) getValue("sTownIDxx");
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setHasRoute(String fsValue) {
+        return setValue("cHasRoute", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getHasRoute() {
+        return (String) getValue("cHasRoute");
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setBlackLst(String fsValue) {
+        return setValue("cBlackLst", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getBlackLst() {
+        return (String) getValue("cBlackLst");
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setRecdStat(String fsValue) {
+        return setValue("cRecdStat", fsValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public String getRecdStat() {
+        return (String) getValue("cRecdStat");
+    }
+    
+    /**
+     * Sets record as active.
+     *
+     * @param fbValue
+     * @return result as success/failed
+     */
+    public JSONObject setActive(boolean fbValue) {
+        return setValue("cRecdStat", fbValue ? "1" : "0");
+    }
+
+    /**
+     * @return If record is active.
+     */
+    public boolean isActive() {
+        return ((String) getValue("cRecdStat")).equals("1");
+    }
+    
+    /**
+     * Description: Sets the Value of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setModified(String fsValue) {
+        return setValue("sModified", fsValue);
+    }
+
+    /**
+     * @return The Value of this record.
+     */
+    public String getModified() {
+        return (String) getValue("sModified");
+    }
+    
+    /**
+     * Sets the date and time the record was modified.
+     *
+     * @param fdValue
+     * @return result as success/failed
+     */
+    public JSONObject setModifiedDte(Date fdValue) {
+        return setValue("dModified", fdValue);
+    }
+
+    /**
+     * @return The date and time the record was modified.
+     */
+    public Date getModifiedDte() {
+        return (Date) getValue("dModified");
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setProvID(String fsValue) {
+        return setValue("sProvIDxx", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getProvID() {
+        return (String) getValue("sProvIDxx");
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setProvName(String fsValue) {
+        return setValue("sProvName", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getProvName() {
+        return (String) getValue("sProvName");
+    }
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setTownName(String fsValue) {
+        return setValue("sTownName", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getTownName() {
+        return (String) getValue("sTownName");
+    }
+    
+    
+    /**
+     * Description: Sets the ID of this record.
+     *
+     * @param fsValue
+     * @return result as success/failed
+     */
+    public JSONObject setZippCode(String fsValue) {
+        return setValue("sZippCode", fsValue);
+    }
+
+    /**
+     * @return The ID of this record.
+     */
+    public String getZippCode() {
+        return (String) getValue("sZippCode");
+    }
+    
+}
